d6a86813ea967a9561227068decfbe156ab17c38
Min Thway Htut <minthwayhtut568@gmail.com>
2025-05-08 21:07:16 +0200

    First release of the project

---
+++ carmarket/listings/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+class Car(models.Model):
+    title = models.CharField(max_length=100)
+    description =  models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    model  = models.CharField(max_length=50)
+    year = models.PositiveIntegerField()
+    image = models.ImageField(upload_to='car_images/')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+    
+
+
