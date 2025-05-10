968276acd91aca0a86280c3b130c7dd5a6ec9daa
Min <minthwayhtut568@gmail.com>
2025-05-10 15:16:21 +0200

    Fixing User Authentication

--- carmarket/carmarket/urls.py
+++ carmarket/carmarket/urls.py
@@ -7,7 +7,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
        path('users/', include('users.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
-]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
