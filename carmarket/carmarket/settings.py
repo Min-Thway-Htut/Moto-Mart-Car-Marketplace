60bfefac34776ba50f8ffca3b8258d26c3897fcb
Min <minthwayhtut568@gmail.com>
2025-05-10 22:58:41 +0200

    Developing a minimun viable product

--- carmarket/carmarket/settings.py
+++ carmarket/carmarket/settings.py
@@ -26,7 +26,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.sites',
     'listings',
-    'users'
+    'users',
+    'widget_tweaks'
 ]
 
 SITE_ID = 1
---
+++ carmarket/users/static/js/car_form_toggle.js
@@ -0,0 +1,4 @@
+function toggleForm() {
+    const section = document.getElementById("uploadSection");
+    section.style.display = section.style.display === "none" ? "block" : "none";
+}
