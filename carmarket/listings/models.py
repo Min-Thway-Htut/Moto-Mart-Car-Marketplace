f3766777705180bb432adea0569355292d7dd190
Min <minthwayhtut568@gmail.com>
2025-05-25 23:55:22 +0200

    Modifying Profile Section

--- carmarket/listings/models.py
+++ carmarket/listings/models.py
@@ -1,6 +1,13 @@
+from django.conf import settings
 from django.db import models
 
+
 class Car(models.Model):
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='car_listings',
+    )
     title = models.CharField(max_length=100)
     description =  models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
