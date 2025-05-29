fe554cc5ec667305c1a3031d0d6a202dda136497
Min <minthwayhtut568@gmail.com>
2025-05-29 12:35:24 +0200

    Adding Email and Phone Number to models.py file

--- carmarket/listings/forms.py
+++ carmarket/listings/forms.py
@@ -4,6 +4,6 @@ from .models import Car
 class CarForm(forms.ModelForm):
     class Meta:
         model = Car
-        fields = ['title', 'description', 'price','model', 'year', 'image' ]
+        fields = ['title', 'description', 'price','model', 'year', 'phone_number','email','image' ]
 
         
--- carmarket/listings/models.py
+++ carmarket/listings/models.py
@@ -14,6 +14,8 @@ class Car(models.Model):
     model  = models.CharField(max_length=50)
     year = models.PositiveIntegerField()
     image = models.ImageField(upload_to='car_images/')
+    phone_number = models.CharField(max_length=20)
+    email = models.EmailField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
